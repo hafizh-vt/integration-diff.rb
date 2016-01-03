719c6b7b395092f9de015c44288de82e93ed4c8c
Yuva <yuva@codemancers.com>
2016-01-03 11:48:49 +0530

    [card-112] add readers for module attributes

--- lib/integration_diff.rb
+++ lib/integration_diff.rb
@@ -10,11 +10,17 @@ module IntegrationDiff
   def self.base_uri=(uri)
     @@base_uri = uri
   end
+  def self.base_uri
+    @@base_uri
+  end
 
   # configure project name to which images belong to.
   @@project_name = "idf"
   def self.project_name=(name)
     @@project_name = name
+  end
+  def self.project_name
+    @@project_name
   end
 
   # configure api_key required to authorize api access
@@ -22,11 +28,17 @@ module IntegrationDiff
   def self.api_key=(key)
     @@api_key = key
   end
+  def self.api_key
+    @@api_key
+  end
 
   # configure js driver which is used for taking screenshots.
   @@javascript_driver = "poltergeist"
   def self.javascript_driver=(driver)
     @@javascript_driver = driver
+  end
+  def self.javascript_driver
+    @@javascript_driver
   end
 
   # configure service to be mocked so that no screenshots are
@@ -35,11 +47,17 @@ module IntegrationDiff
   def self.enable_service=(enable)
     @@enable_service = enable
   end
+  def self.enable_service
+    @@enable_service
+  end
 
   # configure logger, which will be used to log issues if any
   @@logger = Logger.new(STDOUT)
   def self.logger=(new_logger)
     @@logger = new_logger
+  end
+  def self.logger
+    @@logger
   end
 
   # helper to configure above variables.
