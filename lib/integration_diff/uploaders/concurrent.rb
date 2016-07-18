2a9f37894771d6a27b369fdefaa861c5a8e24b94
Yuva <yuva@codemancers.com>
2016-07-18 22:29:43 +0530

    [card-92] ensure all screenshots taken are uploaded

--- lib/integration_diff/uploaders/concurrent.rb
+++ lib/integration_diff/uploaders/concurrent.rb
@@ -9,15 +9,28 @@ module IntegrationDiff
       def initialize(run_id)
         @run_id = run_id
         @pool = ::Concurrent::FixedThreadPool.new(MAX_NO_OF_THREADS)
+        @screenshots_taken = 0
       end
 
       def enqueue(identifier)
+        @screenshots_taken += 1
+
         @pool.post do
           IntegrationDiff::Utils.upload_image(@run_id, identifier)
         end
       end
 
       def wrapup
+        retries = 180           # 30 mins
+
+        # if all screenshots are not uploaded, wait.
+        until @screenshots_taken == @pool.completed_task_count
+          retries -= 1
+          break if retries.zero?
+
+          sleep 10
+        end
+
         @pool.shutdown
       end
     end
